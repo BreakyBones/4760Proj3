05820b61debb44698022fef1fd76a035cb3b9c87
Kanaan <krs8dd@umsystem.edu>
2024-03-09 22:11:16 -0600

    Added message handling and recieving

--- oss.c
+++ oss.c
@@ -228,10 +228,6 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    // initialize worker variables
-    int activeWorkers = 0; // active workers
-    int endedWorkers = 0; // number of workers ended
-    int timeout = 1; // Loop variable
 
     arg_i *= 1000000;
     int launchTimeS;
@@ -241,21 +237,96 @@ int main(int argc, char** argv) {
         launchTimeN -= 1000000000;
     }
 
+    for( int i = 0; i < arg_s; i++) {
+        pid_t childPid = fork();
+
+        if (childPid == 0) {
+            generateTime(arg_t, &randomSeconds, &randomNano);
+
+            char randomSecondsBuffer[20], randomNanoSecondsBuffer[20];
+            sprintf(randomSecondsBuffer, "%d", randomSeconds);
+            sprintf(randomNanoSecondsBuffer, "%d", randomNano);
+
+            char* args[] = {"./worker" , randomSecondsBuffer , randomNanoSecondsBuffer, 0};
+
+            // execute worker process
+            execvp(args[0], args);
+        } else {
+            for(int i = 0; i < arg_n; i++) {
+                if (processTable[i].pid == 0) {
+                    processTable[i].occupied = 1;
+                    processTable[i].pid = childPid;
+                    processTable[i].startSeconds = clockPointer->seconds;
+                    processTable[i].startNano = clockPointer->nanoseconds;
+                    break;
+                }
+            }
+        }
+    }
+
+    // initialize worker variables
+    int activeWorkers = arg_s; // active workers
+    int totalWorkers = 0; // number of workers ended
+    int terminatedWorkers = 0;
+    int timeout = 1; // Loop variable
 
     while(!timeout) {
         IncrementClock(clockPointer);
 
-        if(activeWorkers < arg_s && clockPointer->seconds >= launchTimeS && clockPointer->nanoseconds >= launchTimeN) {
-            launchTimeN = clockPointer->nanoseconds + arg_i;
-            if (clockPointer->nanoseconds >= 1000000000) {
-                launchTimeS = clockPointer->seconds + 1;
-                launchTimeN -= 1000000000;
+        struct PCB childP = processTable[totalWorkers];
+        int cPid = childP.pid;
+
+        if (cPid != 0 && childP.occupied == 1) {
+            msg.mtype = cPid;
+
+            if (msgsnd(msqid, &msg, sizeof(msgbuffer)-sizeof(long), 0) == -1) {
+                perror("oss.c: msgsnd to worker failed");
+                exit(1);
+            } else {
+                char newBuffer[20] , buffer1[20], buffer2[20], buffer3[20];
+                sprintf(newBuffer, "%d" , totalWorkers);
+                sprintf(buffer1 , "%d" , cPid);
+                sprintf(buffer2 , "%d" , clockPointer->seconds);
+                sprintf(buffer3 , "%d" , clockPointer->nanoseconds);
+
+                char message[256];
+                sprintf(message, "OSS: Sending message to worker %s PID: %s at time %s:%s", newBuffer , buffer1 , buffer2 , buffer3);
+                printf("%s\n",message);
+                log(arg_f, message);
             }
 
-            pid_t workPid = fork();
-            if (workPid == 0) {
+            msgbuffer recieve;
+            if (msgrcv(msqid , &recieve, sizeof(msgbuffer) , getpid(), 0) == -1) {
+                perror("oss.c: failed to recieve message\n");
+                exit(1);
+            } else {
+                char newBuffer1[20], buffer4[20], buffer5[20], buffer6[20];
+                sprintf(newBuffer1, "%d", totalWorkers);
+                sprintf(buffer4, "%d" , cPid);
+                sprintf(buffer5, "%d" , clockPointer->seconds);
+                sprintf(buffer6, "%d" , clockPointer->nanoseconds);
 
+                char message2[20];
+                sprintf(message2, "OSS: Recieving message from worker %s PID: %s at time %s:%s\n" , newBuffer1 ,buffer4, buffer5, buffer6);
+                printf("%s\n" , message2);
+                log(arg_f , message2);
             }
+        }
+
+        int status;
+        int terminatingPid = waitpid(-1, &status, WNOHANG);
+
+        if (terminatingPid !=0) {
+            terminatedWorkers++;
+
+            for(int i =0; i < arg_n; i++) {
+                if(processTable[i].pid == terminatingPid) {
+                    processTable[i].occupied = 0;
+                    break;
+                }
+            }
+
+            if(workers < proc)
         }
     }
 }
